ebd3a525e0100f87ff358fa83b3bf2cf9ab2eedc
Harkiran Bhullar <hb9633@desales.edu>
2022-11-08 10:34:15 -0500

    as

--- app/src/main/java/com/hfad/vacationdestination/MyAdapter.java
+++ app/src/main/java/com/hfad/vacationdestination/MyAdapter.java
@@ -30,6 +30,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.vaction_item, parent, false);
 
+
+        System.out.println("Done Creating view");
         return new MyViewHolder(view);
 
     }
@@ -44,7 +46,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         VacationDestiation vd = destinationList.get(position);
 
-        holder.setData(vd);
+        System.out.println("Done populating row: " + position+  vd.getPlaceName());
+        holder.setData(vd,position);
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder
@@ -52,19 +55,53 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
 
         private TextView tvName;
         private ImageView imgvDestination;
+        private int currentPosition = -1;
+        private VacationDestiation currentDestination = null;
+        private  ImageView imvDelete;
+        private  ImageView imvMakeCopy;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             imgvDestination = itemView.findViewById(R.id.imvPlace);
             tvName =  itemView.findViewById(R.id.tvPlaceName);
+            imvDelete = itemView.findViewById(R.id.imvDelete);
+            imvMakeCopy = itemView.findViewById(R.id.imvMakeCopy);
+
+            imvDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    destinationList.remove(currentPosition);
+
+                    notifyItemRemoved(currentPosition);
+                    notifyItemRangeChanged(currentPosition,destinationList.size());
+
+                }
+            });
+
+            imvMakeCopy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    destinationList.add(currentPosition,currentDestination);
+
+
+                    notifyItemInserted(currentPosition);
+                    notifyItemRangeChanged(currentPosition,destinationList.size());
+                }
+            });
+
 
         }
 
-        public void setData(VacationDestiation vd)
+        public void setData(VacationDestiation vd,int pos)
         {
             imgvDestination.setImageResource(vd.getImageID());
             tvName.setText(vd.getPlaceName());
+            currentPosition = pos;
+            currentDestination = vd;
+
         }
 
     }
